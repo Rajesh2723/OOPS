f64ec923f945d3588d7c3d677000555dff2442a5
Rajesh G <92634862+Rajesh2723@users.noreply.github.com>
2023-06-21 18:54:06 +0530

    Create transferfunds.php

---
+++ TSF/transferfunds.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php include('header.php');?>
+    <body id="transfer-done">
+        <h1 class="text-center fw-bold">Goodwill Banks</h1>
+        <div id="transferdiv" class="container-fluid text-center">
+            <p class="fs-3">Your transaction was successful!</p>
+            <a href="index.php" class="btn btn-success shadow-none">Back to homepage</a>
+        </div>
+    <?php include('footer.php');?>
