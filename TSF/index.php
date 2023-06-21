d45ed153bc65bee8574fcdac026965f35f4c5e97
Rajesh G <92634862+Rajesh2723@users.noreply.github.com>
2023-06-21 18:52:50 +0530

    Create index.php

---
+++ TSF/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php include('header.php');?>
+    <body id="index">
+    <div class="container-fluid">
+        <h1 class="text-center fw-bolder">Goodwill Banks</h1>
+        <div id="welcome" class="text-center">
+            <h2 class="display-4 fw-bold">Welcome!</h2>
+            <p class="lead fw-bold">
+                Now you can transfer funds to your loved ones via our banking system.
+            </p>
+            <p class="lead1">So let's get started!</p>
+            <a class="btn btn-success shadow-none" href="customer.php">View Customer Information</a>
+        </div>
+    </div>
+    <?php include('footer.php');?>
